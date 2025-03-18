c528945598f4460bbe1046b8d77a554d9bc95c9a
n-pontes <nuno06.np@gmail.com>
2025-03-18 08:13:48 +0100

    extract hidden form data from HTML response

--- script.py
+++ script.py
@@ -5,3 +5,8 @@ def convert_text_to_unicode(input_text):
     url = "https://lsgkerala.gov.in/unicode/Default.aspx"  
     session = requests.Session()  
     response = session.get(url)  
+
+if response.status_code == 200:  
+    soup = BeautifulSoup(response.text, "html.parser")  
+    viewstate = soup.find("input", {"name": "__VIEWSTATE"})["value"]  
+    eventvalidation = soup.find("input", {"name": "__EVENTVALIDATION"})["value"]  
