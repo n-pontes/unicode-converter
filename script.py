8ec4af92842e72088231cfff108f405e4ad9e7c9
n-pontes <nuno06.np@gmail.com>
2025-03-18 08:13:00 +0100

    create function and fetch converter page with GET request

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
 import requests  
 from bs4 import BeautifulSoup 
+
+def convert_text_to_unicode(input_text):  
+    url = "https://lsgkerala.gov.in/unicode/Default.aspx"  
+    session = requests.Session()  
+    response = session.get(url)  
